7eeea094d95ef904746fbb2aa679b3cca9f186e9
Congglee <congldqn888@gmail.com>
2024-09-27 13:35:50 +0700

    Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-const name: string = 'Dư Thanh Được'
-console.log(name)
+import { Server } from 'http'
+import app from '~/app'
+
+let server: Server
+
+server = app.listen(3000, () => {
+  console.log('Server is running on port 3000')
+})
+
+process.on('SIGTERM', () => {
+  // logger.info('SIGTERM received')
+  if (server) {
+    server.close()
+  }
+})
