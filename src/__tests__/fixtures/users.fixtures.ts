5c7b6bf0f40ff988e8cb6bcb5d4d2aed9d432a77
Congglee <congldqn888@gmail.com>
2024-11-16 16:26:49 +0700

    Change user email fixture for testing

--- src/__tests__/fixtures/users.fixtures.ts
+++ src/__tests__/fixtures/users.fixtures.ts
@@ -2,7 +2,7 @@ import { faker } from '@faker-js/faker'
 
 export const newUserFixture = {
   name: faker.person.fullName(),
-  email: faker.internet.email(),
+  email: 'johndoe@imail.edu.vn',
   password: 'Test@1234',
   confirm_password: 'Test@1234',
   date_of_birth: faker.date.past().toISOString()
